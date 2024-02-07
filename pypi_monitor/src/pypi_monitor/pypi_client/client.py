5e1b208adf73e246fdd16a38868445c810b1664c
Krpec, Blake C <blake.krpec@swri.org>
2024-02-07 09:56:44 -0600

    client files now flake8 formatted

--- pypi_monitor/src/pypi_monitor/pypi_client/client.py
+++ pypi_monitor/src/pypi_monitor/pypi_client/client.py
@@ -3,13 +3,14 @@ import platform
 from pypi_monitor.pypi_client import windows_client
 from pypi_monitor.pypi_client import linux_client
 
+
 class Client():
     def __init__(self, main_window):
-        #determine if on windows or linux and start appropriate data client
+        # determine if on windows or linux and start appropriate data client
         self.os = platform.system()
         self.main_window = main_window
 
         if self.os == "Linux":
             self.data_client = linux_client.DataClient(self.main_window)
-        elif self.os =="Windows":
+        elif self.os == "Windows":
             self.data_client = windows_client.DataClient(self.main_window)
